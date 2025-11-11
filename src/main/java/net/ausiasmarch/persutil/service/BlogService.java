1511c2c7ef875c75542be3c7bc7e25ab29c40a2e
“rond88” <r0nald19202006@gmail.com>
2025-11-11 20:22:04 +0100

    Refactor BlogService to generate random content for blog posts

--- src/main/java/net/ausiasmarch/persutil/service/BlogService.java
+++ src/main/java/net/ausiasmarch/persutil/service/BlogService.java
@@ -1,6 +1,8 @@
 package net.ausiasmarch.persutil.service;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,22 +12,43 @@ import net.ausiasmarch.persutil.repository.BlogRepository;
 
 @Service
 public class BlogService {
+    public final static int NUM_PALABRAS = 5;
 
     @Autowired
     BlogRepository oBlogRepository;
-    public Long  rellenarBlog() {
+
+    public Long rellenarBlog() {
         // crear un arrayList con 25 palabras aleatorias
+
+        List<String> palabras = Arrays.asList(
+                "sol", "luna", "estrellas", "planeta", "galaxia",
+                "arbol", "flor", "rio", "montaña", "nube",
+                "casa", "coche", "libro", "ordenador", "telefono",
+                "musica", "arte", "historia", "ciencia", "deporte",
+                "familia", "amigo", "trabajo", "escuela", "comida",
+                "viaje", "tiempo", "agua", "fuego", "tierra",
+                "alegria", "paz", "esperanza", "silencio", "sueño");
         
+
         // Lógica para rellenar el blog
         BlogEntity oBlogEntity = new BlogEntity();
         oBlogEntity.setTitulo("Mi primer blog");
-        oBlogEntity.setContenido("Este es el contenido de mi primer blog.");
+        oBlogEntity.setContenido(crearContenidoAleatorio(palabras));
         oBlogEntity.setEtiquetas("etiqueta1, etiqueta2");
         oBlogEntity.setFechaCreacion(LocalDateTime.now());
         oBlogEntity.setFechaModificacion(null);
 
         oBlogRepository.save(oBlogEntity);
-
+    
         return oBlogRepository.count();
+    }
+    // Crear contenido de cinco palabras aleatorias
+    public String crearContenidoAleatorio(List<String> palabras) {
+        StringBuilder contenido = new StringBuilder();
+        for (int i = 0; i < NUM_PALABRAS; i++) {
+            int indiceAleatorio = (int) (Math.random() * palabras.size());
+            contenido.append(palabras.get(indiceAleatorio)).append(" ");
+        }
+        return contenido.toString();
     }
 }
